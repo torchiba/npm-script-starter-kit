40d5f9d8537c46c4ae104cd5a849dbc46f70dd0f
torchiba <account@torch-design.work>
2021-05-12 19:24:11 +0900

    [modify] script.js - エラー

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,38 +1,39 @@
 (() => {
-  let resizeRunning;
-  let scrollRunning;
   // -------------------------------- //
   // リサイズ・スクロールごとの処理
   // -------------------------------- //
-  // リサイズするたびに処理走らせる
-  resizeRunning = false;
-  window.addEventListener('resize', function() {
-    // 呼び出されるまで何もしない
-    if (!resizeRunning) {
-      isRunning = true;
-      // 描画する前のタイミングで呼び出してもらう
-      window.requestAnimationFrame(function() {
-        // ここでなにか処理をする
-        console.log('resizeRunning');
-        resizeRunning = false;
-      });
-    }
+  window.addEventListener('load', () => {
+    // ---------------- resizeRunning ---------------- //
+    let resizeRunning = false;
+    window.addEventListener('resize', () => {
+      // 呼び出されるまで何もしない
+      if (!resizeRunning) {
+        resizeRunning = true;
+        // 描画する前のタイミングで呼び出してもらう
+        window.requestAnimationFrame(() => {
+          // ここでなにか処理をする
+          console.log('resizeRunning');
+          resizeRunning = false;
+        });
+      }
+    });
+
+    // ---------------- scrollRunning ---------------- //
+    let scrollRunning = false;
+    window.addEventListener('scroll', () => {
+      // 呼び出されるまで何もしない
+      if (!scrollRunning) {
+        scrollRunning = true;
+        // 描画する前のタイミングで呼び出してもらう
+        window.requestAnimationFrame(() => {
+          // ここでなにか処理をする
+          console.log('scrollRunning');
+          scrollRunning = false;
+        });
+      }
+    });
   });
 
-  // スクロールするたびに処理走らせる
-  scrollRunning = false;
-  window.addEventListener('scroll', function() {
-    // 呼び出されるまで何もしない
-    if (!scrollRunning) {
-      scrollRunning = true;
-      // 描画する前のタイミングで呼び出してもらう
-      window.requestAnimationFrame(function() {
-        // ここでなにか処理をする
-        console.log('scrollRunning');
-        scrollRunning = false;
-      });
-    }
-  });
   // -------------------------------- //
   // ライブラリ読み込み
   // -------------------------------- //
